02ae354c4f7d3cec7319abe6ebe86f9bef2557e9
tmxichalakes <tmichalakes@themxgroup.com>
2026-03-03 16:15:41 -0700

    replaced test components with a real one

--- custom/bundle-builder/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import BundleBuilder from './BundleBuilder'
-
-createRoot(document.getElementById('bundle-root')!).render(
-  <StrictMode>
-    <BundleBuilder />
-  </StrictMode>,
-);
--- custom/foo/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Foo from './Foo'
-
-createRoot(document.getElementById('foo-root')!).render(
-  <StrictMode>
-    <Foo fooName="My Foo" />
-  </StrictMode>,
-);
