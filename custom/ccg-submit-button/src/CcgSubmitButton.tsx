ea8bb33b922ad86de2d80f017e2a4821da5b112e
tmxichalakes <tmichalakes@themxgroup.com>
2026-03-04 09:16:16 -0700

    CCG Submit Button

--- custom/ccg-submit-button/src/CcgSubmitButton.tsx
+++ custom/ccg-submit-button/src/CcgSubmitButton.tsx
@@ -1,7 +1,47 @@
-export default function CcgSubmitButton() {
+export type CcgSubmitButtonProps = {
+	dataProductFormId: string
+};
+
+export default function CcgSubmitButton({ dataProductFormId }: CcgSubmitButtonProps) {
+	
+	
+	const addGuidAndSubmit = (formdata: any) => {
+	
+		const form =
+			document.querySelector('[data-product-form]') ||
+			document.querySelector('form[action^="/cart/add"]');
+
+		if(!form)
+			throw new Error("Couldn't find the form");
+
+		const formData = new FormData(form as HTMLFormElement);
+	
+		
+
+		submit(formdata)
+			.then(() => {
+				console.log("I did it!");
+			})
+			.catch((err) => {
+				console.error("I messed up", err);
+			})
+	};
+
+	const submit = async (formdata: any) => {
+		const response = await fetch("/cart/add.js", {
+			method: "POST",
+			body: formdata,
+			headers: {
+				"Accept": "application/json"
+			}
+		});
+	}
+
 	return (
 		<div>
-			<h2>Here's a submit button</h2>
+			<button
+				onClick={addGuidAndSubmit}
+			>Submit</button>
 		</div>
 	)
 }
